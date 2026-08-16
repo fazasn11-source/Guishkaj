97486c75e667b2798f72f9f0da3b55d54e8815ee
fazasn11-source <fazasn11@gmail.com>
2026-08-17 04:29:22 +0500

    Fix build.gradle.kts Kotlin DSL syntax (convert from Groovy DSL)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,26 @@
-plugins { id 'fabric-loom' version '1.3.0' }
+plugins {
+    id("fabric-loom") version "1.3.0"
+}
 
 repositories {
     mavenCentral()
-    maven { url 'https://maven.fabricmc.net/' }
+    maven {
+        url = uri("https://maven.fabricmc.net/")
+    }
 }
 
-group = 'com.guishkaj'
-version = '0.1.0'
-archivesBaseName = 'guishkaj'
+group = "com.guishkaj"
+version = "0.1.0"
+
+base {
+    archivesName.set("guishkaj")
+}
 
 dependencies {
-    minecraft "com.mojang:minecraft:1.21.4"
-    mappings "net.fabricmc:yarn:1.21.4+build.1:v2"
+    minecraft("com.mojang:minecraft:1.21.4")
+    mappings("net.fabricmc:yarn:1.21.4+build.1:v2")
 
-    modImplementation "net.fabricmc:fabric-api:0.89.0+1.21.4"
+    modImplementation("net.fabricmc:fabric-api:0.89.0+1.21.4")
 }
 
 java {
@@ -25,6 +32,6 @@ loom {
     runs {}
 }
 
-tasks.withType(JavaCompile) {
-    options.encoding = 'UTF-8'
+tasks.withType<org.gradle.api.tasks.compile.JavaCompile> {
+    options.encoding = "UTF-8"
 }
