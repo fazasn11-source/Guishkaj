e453150c6abeeea9dda6e22bc61bb882bd36126a
fazasn11-source <fazasn11@gmail.com>
2026-08-17 04:42:06 +0500

    Add settings.gradle.kts in project subdirectory so fabric-loom resolves when building in that folder

---
+++ Guishkaj/settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.fabricmc.net/")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net/")
+    }
+}
+
+rootProject.name = "Guishkaj"
